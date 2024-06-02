40af5dfb33b5a6a77eb5f8ee68c712bc639e414a
BaraaMohamed2311 <baraamohamed2311@gmail.com>
2024-06-02 20:45:22 +0300

    cards

--- Client/src/components/Cards/Cards.jsx
+++ Client/src/components/Cards/Cards.jsx
@@ -2,15 +2,15 @@
 import "./cards.css"
 
 function Cards({ imgPath, title, iscert, link }) {
-  console.log(`${import.meta.env.BASE_URL}${imgPath}`)
+  const imagePath = `${import.meta.env.BASE_URL}${imgPath.startsWith('/') ? imgPath.slice(1) : imgPath}`;
   return (
     <div className={iscert ? 'cert-card' : 'card'}>
       {iscert ? (
         <a  rel="noreferrer" className='card-link' target="_blank" href={link}>
-          <img className='card-img' src={`/Client/${imgPath}`} alt="Skills_img" />
+          <img className='card-img' src={imagePath} alt="Skills_img" />
         </a>
       ) : (
-        <img className='card-img' src={`/${imgPath}`} alt="Skills_img" />
+        <img className='card-img' src={imagePath} alt="Skills_img" />
       )}
       <h3 className='card-title' data-title={title}>{title}</h3>
     </div>
