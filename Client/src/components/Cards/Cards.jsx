05e1132ac6d79eb01410c78d6ba53918bce8858e
BaraaMohamed2311 <baraamohamed2311@gmail.com>
2024-06-04 14:26:50 +0300

    height attr removed

--- Client/src/components/Cards/Cards.jsx
+++ Client/src/components/Cards/Cards.jsx
@@ -6,10 +6,10 @@ function Cards({ imgPath, title, iscert, link }) {
     <div className={iscert ? 'cert-card' : 'card'}>
       {iscert ? (
         <a  rel="noreferrer" className='card-link' target="_blank" href={link}>
-          <img  width="160"  height="160"  loading="lazy" className='card-img' src={`./${imgPath}`} alt="Skills_img" />
+          <img  width="160"    loading="lazy" className='card-img' src={`./${imgPath}`} alt="Skills_img" />
         </a>
       ) : (
-        <img  width="160" height="160" loading="lazy" className='card-img' src={`./${imgPath}`} alt="Skills_img" />
+        <img  width="160"  loading="lazy" className='card-img' src={`./${imgPath}`} alt="Skills_img" />
       )}
       <h3 className='card-title' data-title={title}>{title}</h3>
     </div>
