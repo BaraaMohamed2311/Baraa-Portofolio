801740d6f90075f5c2fef074484301d5efcc6d15
baraa mohammed <baraamohammed2311@gmail.com>
2024-06-15 15:50:54 +0300

    deploy.yml & server

---
+++ Admin/server.js
@@ -0,0 +1,42 @@
+const express =  require("express");
+const express =  express();
+const PORT = 8080;
+
+app.use(express.json())
+
+app.post("/",async (req , res)=>{
+    
+        const {email , text} =req.body;
+        try{
+            const isSent =await mailSupportHelpers(email , text);
+            
+            if(isSent){
+                res.json({
+                    success:true,
+                    message:"Email Sent Successfully"
+                });
+            }
+            else{
+                res.json({
+                    success:false,
+                    message:"Email wasn't Sent"
+                });
+            }
+            
+        }
+        catch(err){
+            console.log("Error Sending Email Support" , err)
+            res.json({
+                success:false,
+                message:"Error Sending Email"
+            });
+        }
+    
+
+})
+
+
+app.listen(PORT,function(err){
+    if (err) console.log("Error in server setup")
+    console.log("Server listening on Port", PORT);
+})
