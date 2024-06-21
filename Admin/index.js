be5b2f2398613adf57de489576a70b481e4543a8
baraa mohammed <baraamohammed2311@gmail.com>
2024-06-22 01:39:45 +0300

    enable cors

--- Admin/index.js
+++ Admin/index.js
@@ -1,7 +1,10 @@
 const express =  require("express");
+const cors = require('cors')
 const app =  express();
 const PORT = 8080;
 
+
+app.use(cors())
 app.use(express.json())
 
 app.get("/api/",(req , res)=>{
